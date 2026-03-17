d74e296c062f54cb28f9e6a06205e393d5c7de80
Alisa Hamilton <67701544+alisahamilton1@users.noreply.github.com>
2026-03-17 15:31:17 -0400

    Delete src/geopops directory

--- src/geopops/__init__.py
+++
@@ -1,8 +0,0 @@
-from .config import WriteConfig
-from .download_data import DownloadData
-from .census import ProcessData
-from .julia import RunJulia
-from .geopops_starsim import ForStarsim
-
-__all__ = ["WriteConfig", "DownloadData", "ProcessData", "RunJulia", "ForStarsim"]
-
